8dd788efa2122d28ac31687b6f76ab309145d5f5
ME0094 <124847072+ME0094@users.noreply.github.com>
2024-05-07 20:25:59 +0200

    Creation of dll stats

---
+++ dll_stats.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   dll_stats.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/02 12:53:46 by antgalan          #+#    #+#             */
+/*   Updated: 2024/05/07 20:23:54 by martirod         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "datalib.h"
+
+int	dll_empty(t_dlist *list)
+{
+	// TODO
+}
+
+int	dll_size(t_dlist *list)
+{
+	// TODO
+}
+
+t_dlist	*dll_first(t_dlist *list)
+{
+	// TODO
+}
+
+t_dlist	*dll_last(t_dlist *list)
+{
+	// TODO
+}
+
+t_dlist	*dll_search(t_dlist *list, void *data)
+{
+	// TODO
+}

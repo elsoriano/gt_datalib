0dfa5339a0bc512c9ad32b0b130752c6c40ec4fb
ME0094 <124847072+ME0094@users.noreply.github.com>
2024-05-07 20:28:53 +0200

    Creation of dll_deleters,c

---
+++ dll_deleters.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   dll_deleters.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/02 12:53:46 by antgalan          #+#    #+#             */
+/*   Updated: 2024/05/07 20:28:27 by martirod         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "datalib.h"
+
+void	dll_remove_first(t_dlist **list)
+{
+	// TODO
+}
+
+void	dll_remove(t_dlist **list, t_dlist *elem)
+{
+	// TODO
+}
+
+void	dll_remove_last(t_dlist **list)
+{
+	// TODO
+}
+
+void	dll_purge(t_dlist **list, void *data)
+{
+	// TODO
+}
+
+void	dll_clear(t_dlist **list)
+{
+		// TODO
+}
